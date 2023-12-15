10bd8aca4b80163f8e0210193f8202e238554db9
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-12-15 23:20:29 +0100

    add tailwind and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
-const app = () => {
-  return (
-    <div>app</div>
-  )
+const App = () => {
+    return (
+        <main className="bg-slate-300/20">
+            <Router>
+                Navbar
+                <Routes>
+                    <Route path="/" element={"Home"}></Route>
+                    <Route path="/projects" element={"Projects"}></Route>
+                    <Route path="/about" element={"About"}></Route>
+                    <Route path="/contact" element={"Contact"}></Route>
+                </Routes>
+            </Router>
+        </main>
+    )
 }
 
-export default app
+export default App
