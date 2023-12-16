9a112245754ce19118dfeec35821fe0799a5778c
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-12-16 15:33:25 +0100

    adding pages layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import {Home,Contact,About,Projects} from "./pages"
+
 
 const App = () => {
     return (
@@ -7,10 +9,10 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={"Home"}></Route>
-                    <Route path="/projects" element={"Projects"}></Route>
-                    <Route path="/about" element={"About"}></Route>
-                    <Route path="/contact" element={"Contact"}></Route>
+                    <Route path="/"  element={<Home />}></Route>
+                    <Route path="/projects" element={<Projects />}></Route>
+                    <Route path="/about" element={<About />}></Route>
+                    <Route path="/contact" element={<Contact />}></Route>
                 </Routes>
             </Router>
         </main>
