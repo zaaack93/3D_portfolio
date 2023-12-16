5df13bcbe955560f6b091842c715ee7db59fb881
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-12-16 17:27:46 +0100

    SKY

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,43 @@
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
 import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   Popup
 </div> */}
 
 const Home = () => {
+
+const adjustIslandForScreenSize = () =>{
+  let screenScale = null
+  let screenPosition = [0,-6.5,-43]
+  let rotation =[0.1,4.7,0]
+
+  if(window.innerWidth<768){
+    screenScale=[0.9,0.9,0.9]
+  }
+  else{
+    screenScale=[1,1,1]
+  }
+
+  return [screenScale,screenPosition,rotation]
+
+}
+
+const [IslandScale,IslandPosition,IslandeRotation] = adjustIslandForScreenSize()
+
   return (
     <section className="w-full h-screen relative">
       <Canvas className='w-full h-screen bg-transparent'
         camera={{near:0.1,far:1000}}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1,1,1]} intensity={2} />
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+
+          <Island position={IslandPosition} scale={IslandScale} rotation={IslandeRotation} />
         </Suspense>
       </Canvas>
     </section>
